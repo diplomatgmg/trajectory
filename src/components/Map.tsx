0b82bcad2913eeb812d2a78c593467edf855264d
Кирилл Попов <diplomatgmg@gmail.com>
2024-04-23 00:12:01 +0300

    Фикс метки на карте

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,8 @@ import React, { type FC, type ReactElement } from 'react'
 import { type TCar } from '../types/types'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import markerIconPng from 'leaflet/dist/images/marker-icon.png'
+import { Icon } from 'leaflet'
 
 interface MapProps {
   car: TCar
@@ -9,11 +11,14 @@ interface MapProps {
 
 const Map: FC<MapProps> = ({ car }): ReactElement => {
   return (
-    <MapContainer center={[car.latitude, car.longitude]} zoom={13} style={{ height: '200px' }}>
+    <MapContainer center={[car.latitude, car.longitude]}
+                  zoom={13}
+                  style={{ height: '200px' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[car.latitude, car.longitude]}>
+      <Marker position={[car.latitude, car.longitude]}
+      icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })} >
         <Popup>
           {car.name}
         </Popup>
